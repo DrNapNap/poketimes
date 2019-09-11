373a6aa6e2ad4f9f5a088bbbf5f37abb4f862ece
DrNapNap <54890080+DrNapNap@users.noreply.github.com>
2019-09-11 13:59:37 +0200

    ep 27

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import Navbar from './components/Navbar'
+import { BrowserRouter, Route } from 'react-router-dom'
+import Home from './components/Home'
+import Contact from './components/Contact'
+import About from './components/About'
 
 export class App extends Component {
   render() {
     return (
-      <div>
-        
+      <BrowserRouter>
+      <div className="App" >
+        <Navbar/>
+        <Route exact path='/Home' component={Home}/>
+        <Route path='/about' component={About}/>
+        <Route path='/Contact' component={Contact}/>
       </div>
+      </BrowserRouter>
     )
   }
 }
